80a561a6c39007d18de375afb73f1a67d14eff01
Denis Bessarab <bessarab.d.p@gmail.com>
2023-08-20 19:49:27 +0200

    Button rozwin logic for realizations section.

---
+++ src/js/realizacje.js
@@ -0,0 +1,15 @@
+const rozwinButton = document.getElementById('rozwin');
+const grid = document.querySelector('.grid');
+const gridWrapper = document.querySelector('.grid-wrapper');
+const gradient = document.querySelector('.gradient');
+const gridHeight = grid.clientHeight;
+
+rozwinButton.addEventListener('click', () => {
+  rozwinFunction();
+});
+
+function rozwinFunction() {
+  gridWrapper.style.height = gridHeight.toString() + 'px';
+  rozwinButton.classList.add('hidden');
+  gradient.style.height = '0';
+}
